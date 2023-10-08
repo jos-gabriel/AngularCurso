1c61ef64533cbe04e988a365479cd6625b0d20d4
jos-gabriel <jos.gabrie@gmail.com>
2023-10-08 07:43:30 -0600

    refactor(bases)HolaMundo Contador

--- AngularUDEMY/bases/src/app/app.component.ts
+++ AngularUDEMY/bases/src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+  public title:string  = 'Mi primera app angular';
+  public counter:number = 10;
+
+  increaseBy(value:number):void{
+    this.counter += value;
+  }
+  resetCounter(){
+    this.counter = 10;
+  }
 }
