d14a7dd66df332f963ee1252db8aba1a204cca5f
jos-gabriel <jos.gabrie@gmail.com>
2023-10-10 23:09:59 -0600

    refacto(Actividad 6) ActividadViewQueries

--- semana2/dia2/ActividadViewQueries/src/app/app.component.ts
+++ semana2/dia2/ActividadViewQueries/src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { SelectorColorComponent } from './selector-color/selector-color.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+
   title = 'ActividadViewQueries';
 
-  getColor() {}
+  @ViewChild('divPrincipal', { static: true }) divPrincipal!: ElementRef
+  @ViewChild(SelectorColorComponent) selectorComponent!: SelectorColorComponent;
+
+  ngAfterViewInit() {
+    this.divPrincipal.nativeElement.style.backgroundColor = this.selectorComponent.colorPorOmision;
+  }
+
+  getColor() {
+    this.divPrincipal.nativeElement.style.backgroundColor = this.selectorComponent.getValorColor();
+  }
 }
